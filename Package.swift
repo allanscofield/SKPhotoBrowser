576a0d1a15c45d3d0a5972cca26be64b1f816e3c
Allan <appvs@icloud.com>
2019-08-15 22:27:50 -0300

    Added Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.0
+//
+//  Package.swift
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SKPhotoBrowser",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "SKPhotoBrowser",
+            targets: ["SKPhotoBrowser"])
+    ],
+    targets: [
+        .target(
+            name: "SKPhotoBrowser",
+            path: "SKPhotoBrowser")
+    ],
+    swiftLanguageVersions: [.v5]
+)
